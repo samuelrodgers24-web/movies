b35243d1a787297ad0d557f53e2f721ce1c003bc
Samuel Rodgers <samuel.rodgers.24@cnu.edu>
2025-11-21 19:00:56 -0500

    file upload structure

--- adminUpload.php
+++ adminUpload.php
@@ -25,6 +25,7 @@ Welcome to <em>uMovies</em>, your destination for information on <a href="movies
 
 <h2>Administrator Access</h2>
 <?php
+// SESSION INFO
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_STRICT);
 session_start();
 if (isset($_SESSION['perma_pass'])) {
@@ -36,15 +37,74 @@ if (isset($_POST['uploaded'])) {
 if (isset($_SESSION['perma_path'])) {
     $filepath = $_SESSION['perma_path'];
 }
-
+    
+// UPLOAD FUNCTIONS
+function create_table( $data, $b=1, $p=4, $s=4 ) {
+    echo "<table border=$b cellpadding=$p cellspacing=$s>";
+    foreach ($data as $item) {
+        echo "<tr><td align='center'> $item </td></tr>";
+    }
+    echo "</table><p>";
+}
+    
+// INITIATE CONNECTION   
 $db = new mysqli( 'localhost', 'uMoviesAdmin', $password, 'umovies' );
 if ($db->connect_errno) {
     echo "<h3>Connection Error</h3>";
 }
 else {
     echo "<h3>Uploading Data File</h3>";
+    $filename = $_FILES['uploaded'][ 'tmp_name' ];
+    $okay = true;
+    // WAS THE FILE UPLOADED?
+    if ($_FILES[ 'uploaded' ][ 'error' ] > 0) {
+        echo 'A problem was detected:<br/>';
+        switch ($_FILES[ 'uploaded' ][ 'error' ]) {
+            case 1: echo '* File exceeded maximum size allowed by server.<br/>'; break;
+            case 2: echo '* File exceeded maximum size allowed by application.<br/>'; break;
+            case 3: echo '* File could not be fully uploaded.<br/>'; break;
+            case 4: echo '* File was not uploaded.<br/>';
+        }
+        $okay = false;
+    }
+    
+    // IS THE MIME TYPE CORRECT?
+    if ($okay && $_FILES[ 'uploaded' ][ 'type' ] != 'text/plain') {
+        echo 'A problem was detected:<br/>';
+        echo '* File is not a text file.<br/>';
+        $okay = false;
+    }
+    
+    // READ & DISPLAY FILE CONTENTS
+    if ($okay){
+        echo 'File uploaded successfully';
+        $file = fopen ( $filename, 'r' );
+        $fileContents = nl2br ( fread ( $file, filesize( $filename )));
+        fclose( $file );
+        echo "<hr/> $fileContents <hr/>";
+    }
+    
+    
+    // CAN THE FILE BE MOVED TO MY FOLDER
+    $filename = 'file.txt';
+    if ($okay) {
+        if (is_uploaded_file($_FILES[ 'uploaded' ][ 'tmp_name' ])) {
+            if (!move_uploaded_file($_FILES[ 'uploaded' ][ 'tmp_name' ], $filename)) {
+                echo 'A problem was detected:</br>';
+                echo '* Could not copy file to final destination.<br/>';
+                $okay = false;
+            }
+        }
+        else {
+            echo 'A problem was detected:<br/>';
+            echo '* File to copy is not an uploaded file.<br/>';
+            $okay = false;
+        }
+    }
     
 }
+    
+
 $db->close();
 ?>
 <p><copyright>Roberto .A. Flores &copy; 2027</copyright></p>
